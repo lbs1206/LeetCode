03ceb972d196a12371f9b78e136bd9e440baf7b4
lbs1206 <lbs1206@gmail.com>
2024-01-10 11:24:38 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (12.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,12 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int result = -1;
-        for(int i =0; i < nums.length; i++){
-            if(nums[i] == target){
-                result = i;
-                break;
+        int left = 0, right = nums.length -1;
+        
+        while(left < right){
+            int mid = (left + right) /2;
+            if(nums[mid] == target){
+                return mid;
             }
+            
+            if(nums[mid + 1] <= nums[right] ){
+                if(nums[mid + 1] <= target && target <= nums[right]){
+                    left  = mid +1;
+                }else {
+                    right = mid -1;
+                }
+            }else{
+                if(nums[left] <= target && target <= nums[mid -1]){
+                    right = mid -1;
+                }else{
+                    left = mid +1;
+                }
+                
+            }
+
         }
-        return result;
+        return nums[left] == target ? left : -1;
     }
 }
